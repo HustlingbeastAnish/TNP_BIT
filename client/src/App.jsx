a00e7729defcbbbddcb83bf8d30e595116540cea
Anish <anishkumarsingh45@gmail.com>
2023-06-26 02:19:10 +0530

    Routing done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,17 +4,36 @@ import "tailwindcss/tailwind.css";
 import Navbar from "./components/Navbar/Navbar";
 import Gallery from "./components/Gallery/Gallery";
 import Landing from "./components/Landing/Landing";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+  Route,
+  Router,
+  Routes,
+  ScrollRestoration,
+} from "react-router-dom";
+
+import { Root } from "postcss";
+import Login from "./components/Login/Login";
 function App() {
-  return (
-    <>
-      <Navbar />
-      <Landing />
-      {/* <Gallery /> */}
-      {/* <div className="flex justify-center items-center">
-        <h2 className="text-red-600"> Sup GLobal</h2>
-      </div> */}
-    </>
+  // Declaration of Router Object
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/">
+        <Route index element={<Landing />}></Route>
+        <Route path="login" element={<Login />}></Route>
+        {/* <Route path="user">
+          <Route index element={<UserProfile />}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="expressions" element={<UserExpressions />}></Route>
+          <Route path="fundcampaigns" element={<UserCampaigns />}></Route>
+          <Route path="donations" element={<UserDonations />}></Route>
+        </Route> */}
+      </Route>
+    )
   );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
