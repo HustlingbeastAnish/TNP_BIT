c3145bde454c0e2b5373237230c4b1f6c9f1ff4a
Anish <anishkumarsingh45@gmail.com>
2023-07-04 22:20:14 +0530

    regex pattern added for grades

--- client/src/components/StudentDashBoard/Profile/Profile.jsx
+++ client/src/components/StudentDashBoard/Profile/Profile.jsx
@@ -12,9 +12,27 @@ const Profile = () => {
     branch: "",
     roll: "",
   });
+
+  const [marks, setmarks] = useState({
+    sem1: "0.00",
+    sem2: "0.00",
+    sem3: "0.00",
+    sem4: "0.00",
+    sem5: "0.00",
+    sem6: "0.00",
+    sem7: "0.00",
+    sem8: "0.00",
+  });
   const handleEdit = (e) => {
     const { name, value } = e.target;
     setupdatedStud({ ...updatedStud, [name]: value });
+  };
+  const marksEdit = (e) => {
+    const { name, value } = e.target;
+    const regex = /^\d(\.\d{0,2})?$/; // Regex pattern for X.XX format
+    if (regex.test(value) || value === "" || value.length > 4) {
+      setmarks({ ...marks, [name]: value.substr(0, 4) });
+    }
   };
   useEffect(() => {
     if (context.user) {
@@ -171,12 +189,13 @@ const Profile = () => {
                   Sem1
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem1"
+                  value={marks.sem1}
+                  id="sem1"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem1 marks"
+                  maxLength={4}
                   required
                 />
               </div>
@@ -188,12 +207,14 @@ const Profile = () => {
                   Sem2
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem2"
+                  value={marks.sem2}
+                  id="sem2"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem2 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -205,12 +226,14 @@ const Profile = () => {
                   Sem3
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem3"
+                  value={marks.sem3}
+                  id="sem3"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem3 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -222,12 +245,14 @@ const Profile = () => {
                   Sem4
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem4"
+                  value={marks.sem4}
+                  id="sem4"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem4 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -239,12 +264,14 @@ const Profile = () => {
                   Sem5
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem5"
+                  value={marks.sem5}
+                  id="sem5"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem5 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -256,12 +283,14 @@ const Profile = () => {
                   Sem6
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem6"
+                  value={marks.sem6}
+                  id="sem6"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem6 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -273,12 +302,14 @@ const Profile = () => {
                   Sem7
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem7"
+                  value={marks.sem7}
+                  id="sem7"
+                  onChange={marksEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem7 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
@@ -290,12 +321,14 @@ const Profile = () => {
                   Sem8
                 </label>
                 <input
-                  name="phone"
-                  value={updatedStud.phone}
-                  id="floating_phone"
-                  onChange={handleEdit}
+                  name="sem8"
+                  value={marks.sem8}
+                  id="sem8"
+                  // onChange={handleEdit}
                   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light"
-                  placeholder="Enter your email"
+                  placeholder="Enter your sem8 marks"
+                  maxLength={4}
+                  pattern="[0-9]*"
                   required
                 />
               </div>
